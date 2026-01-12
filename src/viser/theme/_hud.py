21318641a914310db484f37b4f15cec689032b0c
Ringo Dulay <ringo.dulay@lomby.jp>
2026-01-12 17:13:09 +0900

    add css styling for hud items

--- src/viser/theme/_hud.py
+++ src/viser/theme/_hud.py
@@ -23,5 +23,66 @@ HUD_HTML = """
 		border: 1px solid rgba(255, 255, 255, 0.4); /* Subtle white border for reflection */
 		transition: opacity 0.2s ease-in-out; /* Added for smoother transitions */
 	}
+	.hud-item {
+		display: flex;
+        flex-direction: column;
+        align-items: center;
+	}
+	..speed-gauge {
+		position: relative;
+		width: 150px;
+		height: 150px;
+	}
+	.speed-value {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		font-size: 96px;
+	}
+	.speed-unit {
+		font-size: 24px;
+	}
+	.battery-bar-container {
+		width: 150px;
+		height: 20px;
+		background-color: #333;
+		border-radius: 5px;
+		overflow: hidden;
+	}
+	.battery-bar-fill {
+		width: {self._battery_soc}%;
+		height: 100%;
+		background-color: {battery_fill_color};
+		border-radius: 5px;
+	}
+	.battery-value {
+		font-size: 48px;
+	}
+	.battery-label {
+		font-size: 36px;
+	}
+	.mission-info {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.mission-label {
+		font-size: 24px;
+		margin-bottom: 5px;
+	}
+	.mission-data {
+		font-size: 48px;
+	}
+	.mission-unit {
+		font-size: 24px; /* Smaller font size for units */
+	}
+	.traffic-light {
+		width: 100px;
+		height: 100px;
+		border-radius: 50%;
+		background-color: #808080;
+		border: 2px solid #FFFFFF;
+	}
 </style>
 """
