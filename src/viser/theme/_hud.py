1638a9d135c2056fc89084fd7139864dde7737a6
Ringo Dulay <ringo.dulay@lomby.jp>
2026-01-29 19:44:26 +0900

    a bit smaller notif and stop reason

--- src/viser/theme/_hud.py
+++ src/viser/theme/_hud.py
@@ -102,9 +102,9 @@ HUD_HTML = """
 		border-top: 1px solid rgba(255, 255, 255, 0.2); /* Divider on top */
 	}
 	.notification-msg {
-		font-size: 36px;
+		font-size: 32px;
 		text-align: left;
-		min-height: 40px;
+		min-height: 37px;
 	}
 	.notification-content {
 		display: flex;
