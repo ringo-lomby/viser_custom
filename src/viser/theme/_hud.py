81c6e4b31ee0941ca9cd31b7b253bd734043f1cf
Ringo Dulay <ringo.dulay@lomby.jp>
2026-01-29 19:30:54 +0900

    update font

--- src/viser/theme/_hud.py
+++ src/viser/theme/_hud.py
@@ -102,7 +102,7 @@ HUD_HTML = """
 		border-top: 1px solid rgba(255, 255, 255, 0.2); /* Divider on top */
 	}
 	.notification-msg {
-		font-size: 30px;
+		font-size: 27px;
 		text-align: left;
 		min-height: 35px;
 	}
